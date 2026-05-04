a7ddd50ef7e1bcecb67dfc799a74415ebbc0ae13
008 <bog_fil@icloud.com>
2026-05-04 20:51:52 +0300

    Добавлены команды /pc /rename, добавлена возможность придумывать своё имя Удалена команда /oc Немного изменена структура кода

--- logi_s/logi_s.py
+++ logi_s/logi_s.py
@@ -1,13 +1,18 @@
-﻿import datetime as d, sys, os, platform as p
+﻿import datetime as d, sys, os, platform as p, psutil as ps,GPUtil as g
 
 
 ############################################################
+
+# infoFile = open("data/infoFile.txt","w")
+# nameInput = input ()
+# infoFile.write(nameInput)
+# infoFile.close()
 
 who1="Программа"
 who2="Пользователь"
 who3="Администратор"
 number=0
-file=open("log_data.txt", "w")
+file=open("data/logData.txt", "w")
 
 ############################################################
 
@@ -15,11 +20,29 @@ def console_print(who, message):
     time=d.datetime.now().strftime("%H:%M:%S")
     number = counter()
     log_save(time, who, message, number)
-    print(f"{time} <{number}> <{who}> <<{message}>>")
+    print(f"{time} <{number}> <{who}> {message}")
 
 def log_save(time, who, message, number):
     global file
-    file.write(f"{time} <{number}> <{who}> <<{message}>>\n")
+    file.write(f"{time} <{number}> <{who}> {message}\n")
+
+def log_save_user(time, number, name, who, message):
+    global file
+    file.write(f"{time} <{number}> <{who}> <{name}> {message}\n")
+
+def console_input(who3, text):
+    number = counter()
+    userName=name()
+    time=d.datetime.now().strftime("%H:%M:%S")
+    message=input(f"{time} <{number}> <{who3}> <{userName}> {text}")
+    log_save_user(time, number, userName, who3, message)
+    return message
+
+def name():
+    infoFile = open("data/infoFile.txt","r")
+    userName = infoFile.read()
+    infoFile.close()
+    return userName
 
 def counter():
     global number
@@ -27,29 +50,63 @@ def counter():
     return number
 
 
-def console_input(who3, text):
-    number = counter()
-    time=d.datetime.now().strftime("%H:%M:%S")
-    message=input(f"{time} <{number}> <{who3}> {text}")
-    log_save(time, who3, message, number)
-    return message
-
 def check(message, who1, who3):
     match message:
         case "/help":
             console_print(who1, "/break - Завершение программы")
             console_print(who1, "/calculator - Запуск калькулятора")
+            console_print(who1, "/date - Сегодняшнюю дату")
+            console_print(who1, "/path - Маршрут к программе")
+            console_print(who1, "/pc - Характеристики вашего ПК")
         case "/calculator":
             calculator(who3)
         case "/date":
             console_print(who1, d.datetime.now().date())
         case "/path":
             console_print(who1, sys.path)
-        case "/os":
-            console_print(who1, p.system())
+        case "/pc":
+            pc(who1)
+        case "/rename":
+            rename(who1, who3)
         case "/-":
             console_print(who1, "")
+    return None
 
+def rename(who1, who3):
+    nameInput = console_input(who3, "Введите новое имя: ")
+    if nameInput == "/stop":
+        console_print(who3, "Переименование отменено")
+    else:
+        infoFile = open("data/infoFile.txt","w+")
+        infoFile.write(nameInput)
+        console_print(who3, "Переименование закончено")
+        infoFile.close()
+    return None
+
+def pc(who1):
+    console_print(who1, "=== СИСТЕМА ===")
+    console_print(who1, p.system()+" "+p.release())
+
+    console_print(who1, "===== CPU =====")
+    console_print(who1, p.processor())
+
+    console_print(who1, "===== GPU =====")
+    gpus=g.getGPUs()
+    if not gpus:
+        console_print(who1, "Проблема с поиском вашей видеокарты, возможно её нет или это ошибка программы")
+    else:
+        for gpu in gpus:
+            console_print(who1, "GPU: "+str(gpu.name))
+            console_print(who1, "Память: " + str(gpu.memoryTotal) + " MB")
+
+    console_print(who1, "===== RAM =====")
+    ram=ps.virtual_memory()
+    console_print(who1, "Всего: " + str(round(ram.total / (1024**3), 2)) + " GB")
+
+    console_print(who1, "==== ДИСК ====")
+    disk = ps.disk_usage("/")
+    console_print(who1, "Всего: " + str(round(disk.total / (1024**3), 2)) + " GB")
+    return None
 
 def calculator(who3):
     who="Калькулятор"
@@ -96,6 +153,7 @@ def calculator(who3):
                 res = "Ошибка, значок действия не найден"
 
         console_print(who, res)
+    return None
 
 
 ############################################################
